8bb7e6c58ca79706a43ede6480c430c1ace3b205
waleed-shaikh <waleedsdev@gmail.com>
2023-11-19 09:31:10 +0000

    Code Synced by DhiWise

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { Button } from "./Button";
+export { Img } from "./Img";
+export { Input } from "./Input";
+export { List } from "./List";
+export { ProtectedRoute } from "./ProtectedRoute";
+export { Text } from "./Text";
+export { TextArea } from "./TextArea";
